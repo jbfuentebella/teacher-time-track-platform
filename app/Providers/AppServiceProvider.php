2e1165ef8d89854a35da3ab0a27f731697275b18
Herbert <jbfuentebella25@gmail.com>
2019-02-17 17:12:33 -0500

    change the database schema to support utf8mb4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // utf8mb4 which includes support for storing emojis.
+        Schema::defaultStringLength(191);
     }
 }
