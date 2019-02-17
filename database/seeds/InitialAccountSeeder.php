cb48657d0a7f8659209b93d54debc835313e74d2
Herbert <jbfuentebella25@gmail.com>
2019-02-17 17:20:09 -0500

    apply initial account to the database

---
+++ database/seeds/InitialAccountSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InitialAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('temp_accounts')->insert([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'username' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin123'),
+            'role' => 'admin',
+            'verification_status' => 'verified',
+            'verification_date' => '2019-02-18',
+            'verification_token' => '1234567891234567',
+            'created_at' => '1970-01-01 00:00:01',
+            'updated_at' => '1970-01-01 00:00:01'
+        ]);
+
+        DB::table('accounts')->insert([
+            'temp_account_id' => 1,
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'username' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin123'),
+            'role' => 'admin',
+            'verification_date' => '2019-02-18',
+            'slug' => '1234567891234567',
+            'updated_by' => 0,
+            'created_at' => '1970-01-01 00:00:01',
+            'updated_at' => '1970-01-01 00:00:01'
+        ]);
+    }
+}
